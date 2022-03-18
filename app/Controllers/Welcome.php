b4552fa34315bbe6059532d8888ac362a7d7c054
s-arslaan <shaikharslaan1234@gmail.com>
2022-03-18 13:03:32 +0530

    json return

--- app/Controllers/Welcome.php
+++ app/Controllers/Welcome.php
@@ -5,19 +5,19 @@ namespace App\Controllers;
 class Welcome extends BaseController
 {
     public function index() {
-        // return view('welcome');
-        $res = array(
-            'name' => 'ars',
-            'name2' => 'shaikh',
-            'city' => 'ahmedabad'
-        );
-        header('Content-Type: application/json');
-        return json_encode( $res );
+        return view('welcome');
+        // $res = array(
+        //     'name' => 'ars',
+        //     'name2' => 'shaikh',
+        //     'city' => 'ahmedabad'
+        // );
+        // header('Content-Type: application/json');
+        // return json_encode( $res );
     }
     
-    public function name($name) {
+    public function encrypt($p,$q) {
         // return view('welcome');
         header('Content-Type: application/json');
-        return json_encode( $name );
+        return json_encode( $p );
     }
 }
