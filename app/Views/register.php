2bfcfddc76b1fe3959671a48688fd536abc7f27f
s-arslaan <shaikharslaan1234@gmail.com>
2022-04-09 17:43:55 +0530

    auth

--- app/Views/register.php
+++ app/Views/register.php
@@ -1,10 +1,21 @@
 
-<?= $this->extend("layouts/base_view") ?>
+<!DOCTYPE html>
+<html lang="en">
 
-<?= $this->section("content") ?>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title><?= $title; ?></title>
+</head>
+
+<body>
+
 
 <h1>THIS IS Register</h1>
 
+</body>
+</html>
 
-<?= $this->endSection("content") ?>
 
